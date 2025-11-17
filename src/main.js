526c5225bf4c6e7abebd91c4a36d95aacdd7d01f
ambulanciaguaicara <ambulanciadeguaicara@gmail.com>
2025-11-16 22:01:20 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
-import { auth, onAuthStateChanged } from "./firebase.js";
+import { auth, login } from "./firebase.js";
 
-// Impede acesso sem login
-onAuthStateChanged(auth, user => {
-    if (!user) {
-        console.log("Sem login — redirecionando...");
-        window.location.href = "/login.html";
-        return;
-    }
+const btnLogin = document.getElementById("btnLogin");
+btnLogin.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  const status = document.getElementById("statusMsg");
+  status.innerText = "";
 
-    // Mostra o usuário logado no rodapé
-    const userBox = document.getElementById("usuarioLogado");
-    if (userBox) {
-        userBox.innerHTML = "Usuário: " + (user.email || "Anônimo");
-    }
+  if(!email || !senha){
+    status.innerText = "Preencha e-mail e senha.";
+    return;
+  }
+
+  try {
+    await login(email, senha);
+    window.location.href = "index.html";
+  } catch(err) {
+    status.innerText = err.message;
+  }
 });
