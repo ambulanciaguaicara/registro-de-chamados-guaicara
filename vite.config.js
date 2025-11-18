1205154c8aeb8b6e9ad9adb3fa0f09dc2345f08b
ambulanciaguaicara <ambulanciadeguaicara@gmail.com>
2025-11-18 16:17:46 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    outDir: "dist"
+  server: {
+    host: true,
+    port: 5173
   }
 });
