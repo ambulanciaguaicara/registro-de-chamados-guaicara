f408ce6196d1ea3cf89d9ceac00561008c2499e2
ambulanciaguaicara <ambulanciadeguaicara@gmail.com>
2025-11-18 12:11:55 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,44 +3,29 @@ const CACHE_NAME = "chamados-cache-v1";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
-  "./login.html",
-  "./logout.html",
   "./css/styles.css",
   "./src/main.js",
   "./manifest.json"
 ];
 
-// Instala e faz cache inicial
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// Ativa e limpa caches antigos
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
     )
   );
   clients.claim();
 });
 
-// Intercepta requisições e serve do cache
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
